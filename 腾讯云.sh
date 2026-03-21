eb536d36c87d0aa76a866aa54ca58bb8caee99bd
tanx <562050688@qq.com>
2026-03-21 21:53:07 +0800

    Enhance setup script for Codex and Claude
    Added installation and configuration for Codex and updated Claude setup.

--- 腾讯云.sh
+++ 腾讯云.sh
@@ -9,24 +9,35 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 nvm install node
 node -v
 npm install -g @anthropic-ai/claude-code
+npm install -g @openai/codex
+
+ssh-keygen -t rsa -b 4096 -C "562050688@qq.com"
 
 #配置claude 私钥
-cd /etc/profile.d/
-cat > claude.sh << 'EOF'
+cat >> ~/.bashrc << 'EOF'
 export ANTHROPIC_BASE_URL='https://api.autocode.space'
 export ANTHROPIC_AUTH_TOKEN='sk-xxxxx'
 EOF
 
-cd ~
-claude
-/plugin marketplace add affaan-m/everything-claude-code
-/plugin install everything-claude-code@everything-claude-code
+mkdir -p ~/.codex
 
-#cd /tmp/
-#git clone https://github.com/affaan-m/everything-claude-code.git
-#mkdir -p /root/.claude/rules/
-#cp -r everything-claude-code/rules/common/* ~/.claude/rules/
-#cp -r everything-claude-code/rules/golang/* ~/.claude/rules/
+cat > ~/.codex/config.toml << 'EOF'
+model_provider = "codex-for-me"
+model = "gpt-5.2"
+model_reasoning_effort = "high"
+disable_response_storage = true
+
+[model_providers.codex-for-me]
+name = "codex-for-me"
+base_url = "https://api-vip.codex-for.me/v1"
+wire_api = "responses"
+requires_openai_auth = true
+EOF
+
+cat > ~/.codex/auth.json << 'EOF'
+{
+"OPENAI_API_KEY": "sk-om9Db...C14"
+}
+EOF
 
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
-npm install -g @openai/codex
